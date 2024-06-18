205ab1f3cbcb7f364a492d397a7775a0a287f7bf
nabi.th <nth@gmail.com>
2024-06-18 01:28:39 -0500

    first bs4 module test

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+with open("website.html") as file1:
+    content = file1.read()
+
+soup = BeautifulSoup(content, "html.parser")
+
+anchor_tags = soup.find_all(name="a")
+
+for anchor in anchor_tags:
+    print(anchor.getText())
+
+
